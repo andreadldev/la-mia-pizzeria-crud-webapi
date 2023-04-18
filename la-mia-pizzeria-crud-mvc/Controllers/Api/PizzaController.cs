e52e3b1bb1e81d7a65ae1650c320fb4ceed9aaee
andreadldev <andrea.delisi2@gmail.com>
2023-04-18 15:15:45 +0200

    api get con parametri name e id

--- la-mia-pizzeria-crud-mvc/Controllers/Api/PizzaController.cs
+++ la-mia-pizzeria-crud-mvc/Controllers/Api/PizzaController.cs
@@ -1,6 +1,7 @@
 ﻿using la_mia_pizzeria_crud_mvc.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace la_mia_pizzeria_crud_mvc.Controllers.Api
 {
@@ -9,12 +10,31 @@ namespace la_mia_pizzeria_crud_mvc.Controllers.Api
     public class PizzaController : ControllerBase
     {
         [HttpGet]
-        public IActionResult GetPizzas() 
+        public IActionResult GetPizzas([FromQuery] string? name) 
         {
             using (PizzaContext ctx = new PizzaContext())
             {
-                IQueryable<Pizza> pizzas = ctx.Pizzas;
-                return Ok(pizzas.ToList());
+                var pizzas = ctx.Pizzas
+                .Where(p => name == null || p.Name.ToLower().Contains(name.ToLower()))
+                .ToList();
+
+                return Ok(pizzas);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPost(int id)
+        {
+            using (PizzaContext ctx = new PizzaContext())
+            {
+                var pizza = ctx.Pizzas.FirstOrDefault(p => p.Id == id);
+
+                if (pizza == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(pizza);
             }
         }
     }
