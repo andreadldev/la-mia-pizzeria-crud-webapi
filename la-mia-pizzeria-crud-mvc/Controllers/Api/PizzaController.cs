afc72fcc123dda6e9e57f770da55a84d17d7013f
andreadldev <andrea.delisi2@gmail.com>
2023-04-18 14:41:28 +0200

    Api get

---
+++ la-mia-pizzeria-crud-mvc/Controllers/Api/PizzaController.cs
@@ -0,0 +1,21 @@
+﻿using la_mia_pizzeria_crud_mvc.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_crud_mvc.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPizzas() 
+        {
+            using (PizzaContext ctx = new PizzaContext())
+            {
+                IQueryable<Pizza> pizzas = ctx.Pizzas;
+                return Ok(pizzas.ToList());
+            }
+        }
+    }
+}
